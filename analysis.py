4982578d0ca47efd77b22bf8db0f197fbf445f01
tomdbrophy <tombrophy@mac.com>
2023-05-09 11:14:27 +0100

    Importing data set

--- analysis.py
+++ analysis.py
@@ -1,2 +1,12 @@
 # Python script for analysis of Iris Dataset.
 # Author: Tom Brophy
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Read in the dataset
+df = pd.read_csv("iris.data")
+
+# Print top 5 rows
+print(df.head())
