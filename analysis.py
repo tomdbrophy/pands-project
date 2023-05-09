93ed5666c630f6d0a586d1317792e4deb6b32e96
tomdbrophy <tombrophy@mac.com>
2023-05-09 12:00:48 +0100

    Scatter plot of Sepal Length vs Sepal Width

--- analysis.py
+++ analysis.py
@@ -4,9 +4,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 # Read in the dataset
-df = pd.read_csv("iris.data",header=None)
+df = pd.read_csv("iris.data",header=0, names=['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm', 'Species'])
 
 # Print top 5 rows
 #print(df.head())
@@ -23,5 +24,13 @@ df = pd.read_csv("iris.data",header=None)
 # Checks for missing values
 #print(df.isnull().sum())
 
-data = df.drop_duplicates(subset=4)
-print(data)
+# Checks for duplicate values in species column. Only 3 unique values
+#data = df.drop_duplicates(subset=4)
+#print(data)
+
+# Counts the number of each species recorded
+#print(df.value_counts([4]))
+
+sns.scatterplot(data=df, x='SepalLengthCm', y='SepalWidthCm', hue='Species')
+plt.legend()
+plt.show()
